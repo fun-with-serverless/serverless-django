ba795dafb980de14c3e62787d846fe382ece2035
Efi MK <efi.merdler@gmail.com>
2023-04-07 16:29:34 +0000

    tutorial-3

--- polls/polls_app/urls.py
+++ polls/polls_app/urls.py
@@ -4,4 +4,9 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
+    path("<int:question_id>/", views.detail, name="detail"),
+    # ex: /polls/5/results/
+    path("<int:question_id>/results/", views.results, name="results"),
+    # ex: /polls/5/vote/
+    path("<int:question_id>/vote/", views.vote, name="vote"),
 ]
